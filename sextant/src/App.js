3073f422330b1da94cca6f18eab614aaf9bee8cf
Gael AMOUSSOU <gaelamoussou@topchrono.com>
2022-10-25 16:32:18 +0100

    correction

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React, { Component } from "react";
+import "./css/App.css";
+import Banner from "./Banner";
+import Exhibit from "./Exhibit";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner bannerText="My sextant application" />
+        <div className="submenu">
+          <Exhibit name="Exhibit container 1"></Exhibit>
+          <Exhibit name="Exhibit container 2"></Exhibit>
+          <Exhibit name="Exhibit container 3"></Exhibit>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
