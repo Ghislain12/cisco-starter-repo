1be7c7c1f828709e7b36fb9c2998b0beae6b66c2
Gael AMOUSSOU <gaelamoussou@topchrono.com>
2022-10-25 18:41:32 +0100

    address Ip

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./css/App.css";
 import Banner from "./Banner";
 import Exhibit from "./Exhibit";
-import AddressDisplay from './AddressDisplay';
+import AddressDisplay from "./AddressDisplay";
+import PylonConnector from './PylonConnector';
 
 class App extends Component {
   render() {
@@ -15,6 +16,9 @@ class App extends Component {
           </Exhibit>
           <Exhibit name="Public IPv6 Address">
             <AddressDisplay url="https://api64.ipify.org?format=json" />
+          </Exhibit>
+          <Exhibit name="Pylon Packet Latency">
+            <PylonConnector />
           </Exhibit>
         </div>
       </div>
